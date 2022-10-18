6ad9ba9b5fc7cd903985b7d49dde6c5402f05038
waleedmalik7 <waleedmalik027@gmail.com>
2022-10-18 04:02:29 -0400

    not optimal solution

--- src/valid_anagram.java
+++ src/valid_anagram.java
@@ -1,28 +1,20 @@
-import java.util.HashMap;
-
 class Solution{
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()){
-            return false;
-        }
-        HashMap<Character, Integer> mapS = new HashMap<Character,Integer>();
+        int alphabet[] = new int[26];
         for(int i = 0; i < s.length(); i++){
-            if(mapS.containsKey(s.charAt(i))){
-                mapS.put(s.charAt(i), mapS.get(s.charAt(i)) + 1);
-            }
-            else{
-                mapS.put(s.charAt(i),1);
-            }
+            int a = s.charAt(i) - 'a';
+            alphabet[a]++;
         }
         for(int i = 0; i < t.length(); i++){
-            if(!mapS.containsKey(t.charAt(i))){
+            int a = t.charAt(i) - 'a';
+            alphabet[a]--;
+        }
+        for(int x: alphabet){
+            if(x != 0){
                 return false;
-            }else if(mapS.get(t.charAt(i)) == 1){
-                mapS.remove(t.charAt(i));
-            }else{
-                mapS.put(t.charAt(i),mapS.get(t.charAt(i))-1);
             }
         }
-        return mapS.size() == 0;
+        return true;
+
     }
 }
